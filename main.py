c3c1d70d1cfd2dae18fc63c2186be93c14583933
Bryan Truong <50316257+yung-upgrade@users.noreply.github.com>
2020-09-03 16:15:48 -0700

    added text to speech and speak function

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-import 
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+import smtplib
+
+print("Initializing J.A.R.V.I.S")
+
+#Setting up text-to-speech voice and speaking rate
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+rate = engine.getProperty("rate")
+
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 120)
+
+#Converts text to speech and reads aloud the text put into the function
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+speak("Hello")
