7ac97df5279d4ff0e6f2967b8d0b97e640d2feb7
Bryan Truong <50316257+yung-upgrade@users.noreply.github.com>
2020-09-03 17:38:52 -0700

    added greeting function

--- main.py
+++ main.py
@@ -6,19 +6,33 @@ import webbrowser
 import os
 import smtplib
 
-print("Initializing J.A.R.V.I.S")
+user = "Bryan"
 
 #Setting up text-to-speech voice and speaking rate
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 rate = engine.getProperty("rate")
 
-engine.setProperty("voice", voices[0].id)
-engine.setProperty("rate", 120)
+engine.setProperty("voice", voices[1].id)
+engine.setProperty("rate", 150)
 
 #Converts text to speech and reads aloud the text put into the function
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("Hello")
+#Greets user based on the time of the day
+def greeting():
+    hour = int(datetime.datetime.now().hour)
+
+    if hour>0 and hour<12:
+        speak("Good Morning" + user)
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon" + user)
+    else:
+        speak("Good Evening" + user)
+
+    speak("How may I help you today?")
+
+speak("Initializing Jarvis...")
+greeting()
