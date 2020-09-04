05c5cdd6c78babda0b7f7773f08f42da09c0add4
Bryan Truong <50316257+yung-upgrade@users.noreply.github.com>
2020-09-04 01:46:58 -0700

    added searchYT function

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import wikipedia
 import webbrowser
 import os
 import smtplib
+import requests
+from bs4 import BeautifulSoup
 
 user = "Bryan"
 
@@ -17,15 +19,11 @@ engine.setProperty("voice", voices[0].id)
 engine.setProperty("rate", 200)
 
 # Converts text to speech and reads aloud the text put into the function
-
-
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
 # Greets user based on the time of the day
-
-
 def greeting():
     hour = int(datetime.datetime.now().hour)
 
@@ -66,45 +64,63 @@ def sendEmail(to, content):
     server.sendmail("email@gmail.com")
     server.close
 
-speak("Initializing Jarvis...")
-greeting()
-query = command()
+#Searches youtube and opens the first result
+def searchYT(text):
+    words = text.split()
+    url = "http://www.youtube.com/results?search_query=" + "+".join(words)
+    search_result = requests.get(url).text
+    soup = BeautifulSoup(search_result, "html.parser")
+    videos = soup.select(".ytd-video-renderer")
+    if not videos:
+        raise KeyError("No video found")
+    link = "https://www.youtube.com" + videos[0]["href"]
 
-if "wikipedia" in query.lower():
-    speak("Searching Wikipedia...")
-    #query = query.lower().replace("wikipedia","")
-    results = wikipedia.summary(query, sentences = 3)
-    speak(results)
+    webbrowser.open_new(link)
 
-elif "youtube" in query.lower():
-    webbrowser.open("https://www.youtube.com/")
+def startJarvis():
+    speak("Initializing Jarvis...")
+    greeting()
+    query = command()
 
-elif "open google" in query.lower():
-    webbrowser.open("google.com")
+    if "wikipedia" in query.lower():
+        speak("Searching Wikipedia...")
+        query = query.lower().replace("wikipedia","")
+        results = wikipedia.summary(query, sentences = 3)
+        speak(results)
 
-elif "play music" in query.lower():
-    song_dir= "C:\\Users\\bryan\\Music".encode("utf-8")
-    songs = os.listdir(song_dir)
-    os.startfile(os.path.join(song_dir,songs[1]))
+    elif "youtube" in query.lower():
+        query = query.lower().replace("youtube","")
+        searchYT(query)
 
-elif "whole lotta red" in query.lower():
-    webbrowser.open("https://www.youtube.com/watch?v=jEkmWm08-Ho&list=PLkL41eK4K0zmPE2hRDhahHwmWmHEeedRn")
+    elif "open google" in query.lower():
+        webbrowser.open("google.com")
 
-elif "this is epic" in query.lower():
-    webbrowser.open("https://www.youtube.com/watch?v=dGJlZw4FYgE")
+    elif "play music" in query.lower():
+        song_dir= "C:\\Users\\bryan\\Music".encode("utf-8")
+        songs = os.listdir(song_dir)
+        os.startfile(os.path.join(song_dir,songs[1]))
 
-elif "the time" in query.lower():
-    time = datetime.datetime.now().strftime("%H:%M:%S")
-    speak(f"{user}, the time is {time}")
+    elif "whole lotta red" in query.lower():
+        webbrowser.open("https://www.youtube.com/watch?v=jEkmWm08-Ho&list=PLkL41eK4K0zmPE2hRDhahHwmWmHEeedRn")
 
-elif "send email" in query.lower():
-    try:
-        speak("Who should I send it to")
-        to = command()
-        speak("What do you want to send")
-        content = command()
-        sendEmail(to, content)
-        speak("The email has been sent!")
+    elif "epic" in query.lower():
+        webbrowser.open("https://www.youtube.com/watch?v=dGJlZw4FYgE")
 
-    except Exception as e:
-        print(e)
+    elif "the time" in query.lower():
+        time = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"{user}, the time is {time}")
+
+    elif "send email" in query.lower():
+        try:
+            speak("Who should I send it to")
+            to = command()
+            speak("What do you want to send")
+            content = command()
+            sendEmail(to, content)
+            speak("The email has been sent!")
+
+        except Exception as e:
+            print(e)
+
+if __name__ == "__main__":
+    startJarvis()
