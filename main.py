7a083077883238ad5305d3a430498071328ab433
Bryan Truong <50316257+yung-upgrade@users.noreply.github.com>
2020-09-03 22:59:16 -0700

    added email, music, and many elif statements

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 rate = engine.getProperty("rate")
 
-engine.setProperty("voice", voices[1].id)
-engine.setProperty("rate", 150)
+engine.setProperty("voice", voices[0].id)
+engine.setProperty("rate", 200)
 
 # Converts text to speech and reads aloud the text put into the function
 
@@ -43,25 +43,69 @@ def command():
     r = sr.Recognizer()
 
     with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
         print("Listening...")
         audio = r.listen(source)
 
         try:
             print("Recognizing...")
             query = r.recognize_google(audio)
-            print(f"user said: {query}/n")
+            print(f"user said: {query}")
 
         except Exception as e:
             speak("Can you repeat that?")
             query = None
+            command()
 
     return query
 
+#If I want to send emails use this function
+def sendEmail(to, content):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+    server.login("myemail@gmail.com", "password")
+    server.sendmail("email@gmail.com")
+    server.close
+
 speak("Initializing Jarvis...")
 greeting()
-command()
+query = command()
 
+if "wikipedia" in query.lower():
+    speak("Searching Wikipedia...")
+    #query = query.lower().replace("wikipedia","")
+    results = wikipedia.summary(query, sentences = 3)
+    speak(results)
 
-# for index, name in enumerate(sr.Microphone.list_microphone_names()):
-#     print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+elif "youtube" in query.lower():
+    webbrowser.open("https://www.youtube.com/")
+
+elif "open google" in query.lower():
+    webbrowser.open("google.com")
+
+elif "play music" in query.lower():
+    song_dir= "C:\\Users\\bryan\\Music".encode("utf-8")
+    songs = os.listdir(song_dir)
+    os.startfile(os.path.join(song_dir,songs[1]))
+
+elif "whole lotta red" in query.lower():
+    webbrowser.open("https://www.youtube.com/watch?v=jEkmWm08-Ho&list=PLkL41eK4K0zmPE2hRDhahHwmWmHEeedRn")
+
+elif "this is epic" in query.lower():
+    webbrowser.open("https://www.youtube.com/watch?v=dGJlZw4FYgE")
+
+elif "the time" in query.lower():
+    time = datetime.datetime.now().strftime("%H:%M:%S")
+    speak(f"{user}, the time is {time}")
+
+elif "send email" in query.lower():
+    try:
+        speak("Who should I send it to")
+        to = command()
+        speak("What do you want to send")
+        content = command()
+        sendEmail(to, content)
+        speak("The email has been sent!")
+
+    except Exception as e:
+        print(e)
